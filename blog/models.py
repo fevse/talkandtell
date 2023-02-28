8321af0f552696460957fb1c1f7ba43dce0aa6bd
fevse <fedulov.v.s@gmail.com>
2023-02-28 22:53:40 +0600

    update settings and apps

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,34 @@
 from django.db import models
 from django import forms
 from django.urls import reverse
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 import uuid
-from datetime import date
 from django.shortcuts import render
 
 # Create your models here.
 
+class TTUser(AbstractUser):
+    is_activated = models.BooleanField(
+        default=True, db_index=True, verbose_name='Активация пройдена?'
+        )
+    
+    send_message = models.BooleanField(
+        default=True, verbose_name='Оповещать о новых постах?'
+        )
+    
+    def __str__(self) -> str:
+        return self.username
+    '''
+    def get_absolute_url(self):
+        return reverse('user_detail', args=[str(self.id)])
+    '''
+
+    class Meta(AbstractUser.Meta):
+        pass
+
 
 class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey('TTUser', on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=200, help_text='title of your post')
     body = models.TextField(max_length=1000, help_text='your post')
     post_date = models.DateTimeField(auto_now_add=True)
@@ -22,4 +40,13 @@ class Post(models.Model):
         return reverse('post_detail', args=[str(self.id)])
 
     class Meta:
-    	ordering = ["-post_date", "author"]
+    	ordering = ["-post_date"]
+
+
+class Comment(models.Model):
+    pass
+
+
+
+
+
--- talkandtell/settings.py
+++ talkandtell/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'talkandtell.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'tandt.sqlite3',
     }
 }
 
@@ -128,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'blog.TTUser'
