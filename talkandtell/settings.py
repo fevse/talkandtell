fc585727ba58562bdb6e7a51bc44aa7e28daa8c1
fevse <fedulov.v.s@gmail.com>
2023-03-12 21:48:00 +0600

    fix settings

--- talkandtell/settings.py
+++ talkandtell/settings.py
@@ -139,7 +139,7 @@ LOGIN_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True 
 EMAIL_HOST = 'smtp.gmail.com' 
-EMAIL_HOST_USER = 'fedulov.v.s@gmail.com' 
-EMAIL_HOST_PASSWORD = 'sbevckyojyfoqoah' 
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587 
 
